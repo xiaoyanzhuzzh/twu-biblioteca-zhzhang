b9121a3016ba7848d7f5b634c48ba765d1a91f7b
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-19 00:32:18 +0800

    complete User entity class

--- src/com/twu/biblioteca/entity/User.java
+++ src/com/twu/biblioteca/entity/User.java
@@ -1,7 +1,41 @@
 package com.twu.biblioteca.entity;
 
-/**
- * Created by zhihuizhang on 7/19/15.
- */
 public class User {
+    private String name;
+    private String password;
+    private String type;
+
+    public User() {
+
+    }
+
+    public User(String name, String password, String type) {
+        this.name = name;
+        this.password = password;
+        this.type = type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
