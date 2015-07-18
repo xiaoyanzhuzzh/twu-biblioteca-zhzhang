2afd9d55e924a02499edb698c3e99e75b8ed5c70
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-19 03:00:46 +0800

    add initLibraryTest method

--- test/com/twu/biblioteca/helper/LibraryHelperTest.java
+++ test/com/twu/biblioteca/helper/LibraryHelperTest.java
@@ -1,7 +1,29 @@
 package com.twu.biblioteca.helper;
 
-/**
- * Created by zhihuizhang on 7/18/15.
- */
+import com.twu.biblioteca.entity.Book;
+import com.twu.biblioteca.entity.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryHelperTest {
+    private Libraryhelper libraryHelper;
+
+    @Before
+    public void initLibraryHelper() {
+        libraryHelper = new Libraryhelper();
+    }
+
+    @Test
+    public void initLibraryTest() {
+        User user = new User("zhzhang", "1111", "customer");
+        assertEquals(user.getName(), libraryHelper.initLibrary().getUsers().get(0).getName());
+        assertEquals(user.getPassword(), libraryHelper.initLibrary().getUsers().get(0).getPassword());
+
+        Book book = new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", "July 8, 1999");
+        assertEquals(book.getName(), libraryHelper.initLibrary().getBooks().get(0).getName());
+        assertEquals(book.getAuthor(), libraryHelper.initLibrary().getBooks().get(0).getAuthor());
+
+    }
 }
