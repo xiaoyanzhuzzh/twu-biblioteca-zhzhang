624c6931851b5ab41158bd9942ae4e8118b7fed9
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-19 00:15:44 +0800

    add setter and getter test of User entity

--- test/com/twu/biblioteca/entity/UserTest.java
+++ test/com/twu/biblioteca/entity/UserTest.java
@@ -1,7 +1,27 @@
 package com.twu.biblioteca.entity;
 
-/**
- * Created by zhihuizhang on 7/18/15.
- */
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class UserTest {
+    private User user;
+
+    @Before
+    public void initUser() {
+        user = new User();
+    }
+
+    @Test
+    public void canSetUserNameTest() {
+        user.setName("zhzhang");
+        assertEquals("zhzhang", user.getName());
+    }
+
+    @Test
+    public void canSetUserPasswordTest() {
+        user.setPassword("1111");
+        assertEquals("1111", user.getPassword());
+    }
 }
