6a96604ab33857e7480250afc490bd2438949fbc
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-18 19:14:51 +0800

    add canSetBooksTest method

--- test/com/twu/biblioteca/entity/LibraryTest.java
+++ test/com/twu/biblioteca/entity/LibraryTest.java
@@ -1,7 +1,27 @@
 package com.twu.biblioteca.entity;
 
-/**
- * Created by zhihuizhang on 7/18/15.
- */
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library library;
+
+    @Before
+    public void initLibrary() {
+        library = new Library();
+    }
+
+    @Test
+    public void canSetBooksTest() {
+        List<Book> books = new ArrayList<Book>();
+        books.add(new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", "July 8, 1999"));
+        library.setBooks(books);
+        assertEquals(books, library.getBooks());
+    }
 }
