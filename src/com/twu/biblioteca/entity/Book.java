989dea1f17f02946fabb1c0db16ca9ac9f3581e8
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-18 19:17:22 +0800

    add Book entity class

--- src/com/twu/biblioteca/entity/Book.java
+++ src/com/twu/biblioteca/entity/Book.java
@@ -1,7 +1,42 @@
 package com.twu.biblioteca.entity;
 
-/**
- * Created by zhihuizhang on 7/18/15.
- */
 public class Book {
+
+    private String name;
+    private String author;
+    private String publishedDate;
+
+    public Book() {
+
+    }
+
+    public Book(String name, String author, String publishedDate) {
+        this.name = name;
+        this.author = author;
+        this.publishedDate = publishedDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
 }
