c049d81e816fff3fd86c45fe047e4821c9cf06a9
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-20 00:01:07 +0800

    add borrowed books

--- src/com/twu/biblioteca/service/LibraryService.java
+++ src/com/twu/biblioteca/service/LibraryService.java
@@ -3,12 +3,25 @@ package com.twu.biblioteca.service;
 import com.twu.biblioteca.entity.Book;
 import com.twu.biblioteca.entity.Library;
 import com.twu.biblioteca.entity.User;
+import com.twu.biblioteca.helper.InputReaderHelper;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LibraryService {
-    public Library initLibrary() {
+
+    public List<User> initUsers() {
+
+        List<User> users = new ArrayList<User>();
+        users.add(new User("zhzhang", "1111", "customer"));
+        users.add(new User("yanzi", "1111", "librarian"));
+        users.add(new User("xueqian", "1111", "librarian"));
+
+        return users;
+    }
+
+    public List<Book> initBooks() {
+
         List<Book> books = new ArrayList<Book>();
         books.add(new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", "July 8, 1999"));
         books.add(new Book("Head First Design Patterns", "Eric Freeman", "November 4, 2004"));
@@ -17,12 +30,7 @@ public class LibraryService {
         books.add(new Book("Head First Java", "Kathy Sierra", "February 9, 2005"));
         books.add(new Book("JavaScript: The Good Parts", "Douglas Crockford", "May, 2008"));
 
-        List<User> users = new ArrayList<User>();
-        users.add(new User("zhzhang", "1111", "customer"));
-        users.add(new User("yanzi", "1111", "librarian"));
-        users.add(new User("xueqian", "1111", "librarian"));
-
-        return new Library(books, users);
+        return books;
     }
 
     public String showWelcomeMessage() {
@@ -49,4 +57,61 @@ public class LibraryService {
         }
         System.out.println();
     }
+
+    public void checkOutBook(Library library) {
+
+        List<Book> books = this.getAvailableBooks(library);
+        this.showBooksOfLibrary(books);
+
+        System.out.println("Please Enter the Book Number You Want to Check Out:");
+        int checkOutNumber = Integer.parseInt(InputReaderHelper.getInput());
+
+        if(checkOutNumber < 0 && checkOutNumber > books.size()) {
+
+            System.out.println("\nSelect A Valid Option!\n");
+        } else {
+
+            System.out.println(checkOutNumber - 1);
+            books.get(checkOutNumber -1).setIsBorrowed(true);
+
+            System.out.println(books.get(checkOutNumber - 1).getIsBorrowed());
+            System.out.println(this.getAvailableBooks(library).size());
+
+            List<Book> returnBooks = new ArrayList<Book>();
+            returnBooks.add(books.get(checkOutNumber));
+            System.out.println("Thank you! Enjoy the book");
+        }
+    }
+
+    public List<Book> getAvailableBooks(Library library) {
+
+        List<Book> availableBooks = new ArrayList<Book>();
+        for(Book book: library.getBooks()) {
+
+            if(!book.getIsBorrowed()) {
+
+                availableBooks.add(book);
+            }
+        }
+
+        return availableBooks;
+    }
+
+//    public void CheckOutBook() {
+//
+//        bookLibrary.listBooks(bookLibrary.getAvailableBooks());
+//        System.out.println("Please input one number that you want to checkout :");
+//        int checkoutNumber = Integer.parseInt(InputReader.getInput());
+//
+//        try {
+//            Book checkoutBook = bookLibrary.getAvailableBooks().get(checkoutNumber - 1);
+//            bookLibrary.getAvailableBooks().remove(checkoutNumber - 1);
+//            bookLibrary.setBorrowedBook(checkoutNumber);
+//            checkoutBook.setUser(user);
+//            System.out.println("Thank you! Enjoy the book");
+//        } catch (Exception e) {
+//            System.out.println("That book is not available");
+//            e.getMessage();
+//        }
+//    }
 }
