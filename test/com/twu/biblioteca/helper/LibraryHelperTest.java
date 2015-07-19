2abbc3bfaef4bc092189614c415a17a913253a76
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-19 18:17:21 +0800

    add canShowWelcomeMessageTest code

--- test/com/twu/biblioteca/helper/LibraryHelperTest.java
+++ test/com/twu/biblioteca/helper/LibraryHelperTest.java
@@ -6,17 +6,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class LibraryHelperTest {
+
     private Libraryhelper libraryHelper;
 
     @Before
     public void initLibraryHelper() {
+
         libraryHelper = new Libraryhelper();
     }
 
     @Test
     public void initLibraryTest() {
+
         User user = new User("zhzhang", "1111", "customer");
         assertEquals(user.getName(), libraryHelper.initLibrary().getUsers().get(0).getName());
         assertEquals(user.getPassword(), libraryHelper.initLibrary().getUsers().get(0).getPassword());
@@ -25,5 +29,12 @@ public class LibraryHelperTest {
         assertEquals(book.getName(), libraryHelper.initLibrary().getBooks().get(0).getName());
         assertEquals(book.getAuthor(), libraryHelper.initLibrary().getBooks().get(0).getAuthor());
 
+    }
+
+    @Test
+    public void canShowWelcomeMessageTest() {
+
+        String welcomeMessage = "----- WELCOME TO BIBLIOTECA -----\n";
+        assertEquals(welcomeMessage, libraryHelper.showWelcomeMessage());
     }
 }
