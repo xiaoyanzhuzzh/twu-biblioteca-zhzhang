feddfec146dd9c1ccbb149a6d540c607f2c67845
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-19 18:32:11 +0800

    add canInitLibraryTest

--- test/com/twu/biblioteca/service/LibraryServiceTest.java
+++ test/com/twu/biblioteca/service/LibraryServiceTest.java
@@ -1,7 +1,30 @@
 package com.twu.biblioteca.service;
 
-/**
- * Created by zhihuizhang on 7/19/15.
- */
+import com.twu.biblioteca.entity.Book;
+import com.twu.biblioteca.entity.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryServiceTest {
+
+    private LibraryService libraryService;
+
+    @Before
+    public void initLibraryService() {
+
+        libraryService = new LibraryService();
+    }
+
+    @Test
+    public void canInitLibraryTest() {
+        User user = new User("zhzhang", "1111", "customer");
+        assertEquals(user.getName(), libraryService.initLibrary().getUsers().get(0).getName());
+        assertEquals(user.getPassword(), libraryService.initLibrary().getUsers().get(0).getPassword());
+
+        Book book = new Book("Refactoring: Improving the Design of Existing Code", "Martin Fowler", "July 8, 1999");
+        assertEquals(book.getName(), libraryService.initLibrary().getBooks().get(0).getName());
+        assertEquals(book.getAuthor(), libraryService.initLibrary().getBooks().get(0).getAuthor());
+    }
 }
