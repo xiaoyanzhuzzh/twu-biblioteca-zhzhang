8640cbac6cc7a999ffb9b8656f9c604c11f302d2
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-08-05 00:16:22 +0800

    rename LibraryServiceTest to BookServiceTest

--- test/com/twu/biblioteca/service/BookServiceTest.java
+++ test/com/twu/biblioteca/service/BookServiceTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-public class LibraryServiceTest {
+public class BookServiceTest {
 
     private BookService libraryService;
 
